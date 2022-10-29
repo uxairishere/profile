c1ef7ffc8b0dd808f1aa043a2530b7a0fa633559
Uxair Abbas <uzaaaaaair@gmail.com>
2022-10-29 05:10:32 +0500

    Update email section

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,39 +1,72 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
+
+    const formdata = new FormData();
+    async function sendEmail(event) {
+        event.preventDefault();
+        formdata.append("name", name);
+        formdata.append("email", email);
+        formdata.append("subject", subject);
+        formdata.append("details", message);
+
+        const response = await axios.post(process.env.REACT_APP_FASTAPI_URL, formdata)
+
+        const data = await response.data
+        if(data.status === 'ok'){
+            alert("Email send!")
+        }else{
+            alert("There was an error")
+        }
+
+    }
+
     return (
         <div id="contact" className="contact-main">
             <div className="contact-container">
                 <h1 className="contacth1">Hire Me</h1>
                 <p className="contactP about-desc">I am available for full time, part time and freelance work. Connect with me via email</p>
-                <form>
+                <form onSubmit={sendEmail}>
                     {/* name email */}
                     <div className="row box">
                         <div className="col-md-6">
                             <div class="input-container">
                                 <label>Name</label>
-                                <input type="text" required />
+                                <input type="text" required
+                                onChange={(e) => {setName(e.target.value)}}
+                                 />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div class="input-container">
                                 <label>Email</label>
-                                <input type="text" required />
+                                <input type="text" required 
+                                onChange={(e) => {setEmail(e.target.value)}}
+                                />
                             </div>
                         </div>
                     </div>
                     {/* subject  */}
                     <div class="input-container">
                         <label>Subject</label>
-                        <input type="text" required />
+                        <input type="text" required
+                        onChange={(e) => {setSubject(e.target.value)}}
+                         />
                     </div>
                     {/* message */}
                     <div class="input-container">
                         <label>Message</label>
-                        <textarea type="text" required />
+                        <textarea type="text" required 
+                        onChange={(e) => {setMessage(e.target.value)}}
+                        />
                     </div>
-                    <div style={{textAlign: 'center'}}>
-                    <button className="btn btn-outline-warning submit-button">Send Message</button>
+                    <div style={{ textAlign: 'center' }}>
+                        <button type="submit" className="btn btn-outline-warning submit-button">Send Message</button>
                     </div>
                 </form>
 
