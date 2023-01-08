2225934d304852c9cf608814d277fd18d5be2f3b
Uxair Abbas <uzaaaaaair@gmail.com>
2023-01-09 03:47:24 +0500

    update index 15

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ProjectDetail from './components/Home/ProjectDetail';
 import NotFound from './components/Home/NotFound';
 import { projects } from './components/states/project.state';
 import { PuffLoader } from 'react-spinners';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
@@ -22,8 +22,8 @@ const App = () => {
     <div className={"App"} style={{ margin: "0" }}>
       <Router>
         <Routes>
-          <Route path='/profile' element={<Profile />} />
-          <Route path={process.env.REACT_APP_PUBLIC_URL + '/#/project/:id'} element={<ProjectDetail projects={projects} />} />
+          <Route path={process.env.REACT_APP_PUBLIC_URL + '/'} element={<Profile />} />
+          <Route path={process.env.REACT_APP_PUBLIC_URL + '/project/:id'} element={<ProjectDetail projects={projects} />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
